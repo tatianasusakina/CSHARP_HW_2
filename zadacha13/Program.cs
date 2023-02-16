1123c93a53eedd7e88d89445662b7716900ace33
susakina tatiana <voodoodoll@icloud.com>
2023-02-17 00:15:33 +0300

    Добавили файл zadacha13

---
+++ zadacha13/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 13: Напишите программу, 
+// которая выводит третью цифру заданного 
+// числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = -1;
+
+if (number >= 100)
+{
+  while (number > 999)
+  {
+    number = number / 10;
+  }
+  result = number % 10;
+  Console.WriteLine(result);
+}
+else
+{
+  Console.WriteLine("третьей цифры нет");
+}
