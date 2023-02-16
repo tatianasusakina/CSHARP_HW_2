0d3de6fe8fa62af31573d06348255872cd906d37
susakina tatiana <voodoodoll@icloud.com>
2023-02-16 23:55:37 +0300

    Добавили файл zadacha10

---
+++ zadacha10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Нажмите Enter");
+Console.ReadLine();
+int value = new Random().Next(100, 999);
+Console.WriteLine(value);
+
+int numberTwo = value % 100 / 10;
+Console.WriteLine(numberTwo);
